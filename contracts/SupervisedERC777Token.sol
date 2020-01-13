46b065a72784891646bc0deed0e0959a395e8acb
monsieurleberre <monsieur.leberre@gmail.com>
2020-01-13 18:09:42 +0000

    add default operator

--- contracts/SupervisedERC777Token.sol
+++ contracts/SupervisedERC777Token.sol
@@ -5,9 +5,9 @@ import "./SupervisedToken.sol";
 
 /**
  * @title Simple777Token
- * @dev Very simple ERC777 Token example, where no tokens are pre-assigned.
- * Note they can later distribute these tokens as they wish using `transfer` and other
- * `ERC20` or `ERC777` functions.
+ * @dev Very simple supervised ERC777 Token example, where no tokens are pre-assigned.
+ * Tokens need to be minted by an authorised address, and can then be circulated as usual
+ * through classic `ERC20` or `ERC777` functions.
  * Based on https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/examples/SimpleToken.sol
  */
 contract SupervisedERC777Token is ERC777, SupervisedToken {
@@ -17,13 +17,10 @@ contract SupervisedERC777Token is ERC777, SupervisedToken {
      */
     constructor (string memory name,
                 string memory symbol)
-                public ERC777(name, symbol, new address[](0)) { }
+                public ERC777(name, symbol, _senderAsArray()) { }
 
     /**
-     * @dev See {ERC20-_mint}.
-     *
      * Requirements:
-     *
      * - the caller must have the {MinterRole}.
      */
     function mint(address account, uint256 amount, bytes memory userData, bytes memory operatorData)
@@ -31,5 +28,13 @@ contract SupervisedERC777Token is ERC777, SupervisedToken {
     returns (bool) {
         _mint(msg.sender, account, amount, userData, operatorData);
         return true;
+    }
+
+    /**
+        Only used once at creation to pass the contract creator as the default operator.
+     */
+    function _senderAsArray() private returns (address[] memory) {
+        address[] memory array = new address[](1);
+        array[0] = msg.sender;
     }
 }
--- contracts/WrappedXRP.sol
+++ contracts/WrappedXRP.sol
@@ -5,10 +5,7 @@ import "./SupervisedERC777Token.sol";
 
 /**
  * @title Simple777Token
- * @dev Very simple ERC777 Token example, where all tokens are pre-assigned to the creator.
- * Note they can later distribute these tokens as they wish using `transfer` and other
- * `ERC20` or `ERC777` functions.
- * Based on https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/examples/SimpleToken.sol
+ * @dev Example of instantiation of the SupervisedERC777Token
  */
 contract WrappedXRP is SupervisedERC777Token {
 
